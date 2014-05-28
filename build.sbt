572a19f8a63142f667984c0ee2c9e478cfd7e22b
Ricardo Lazaro de Oliveira <lazaro@99taxis.com>
2014-05-28 20:56:25 -0300

    with maven

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "norm"
 
-version := "1.0-SNAPSHOT"
+version := "1.0.0-SNAPSHOT"
 
 
 resolvers ++= Seq(
--- sample/app/models/Product.scala
+++ sample/app/models/Product.scala
@@ -1,7 +1,8 @@
 package models
 
-import models.frameworks.{NormCompanion, Norm}
 import java.math.BigDecimal
+import models.frameworks.{NormCompanion, Norm}
+
 
 /**
  * Created by ricardo on 4/22/14.
@@ -11,6 +12,7 @@ case class Product(
   var name:        String,
   var description: Option[String],
   var price:       BigDecimal,
+
   var taxRange:    Int,
   var inStock:     Boolean) extends Norm[Product]
 
